9fcb507f5bf13088e0b378e168436741552c4c86
jimmieswaggthorne <jimmieswaggthorne@gmail.com>
2025-05-28 17:22:50 -0500

    updated files

--- client/src/services/FormServices.ts
+++ client/src/services/FormServices.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : '/';
 
 export const getQuiz = (category: string, difficulty: string) => {
-  return axios.get('/api/questions?category=' + category + '&difficulty=' + difficulty)
+  return axios.get('api/questions?category=' + category + '&difficulty=' + difficulty)
 }
 
 export const getCategories = () => {
-  return axios.get('/api/categories')
+  return axios.get('api/categories')
 }
 
 export const submitQuiz = (submission: any) => {
   let newObject = { name: 'Jimmie Swaggthorne', email: 'jimmieswaggthorne@gmail.com', submission }
-  return axios.post('/api/submit', newObject)
+  return axios.post('api/submit', newObject)
     .then(response => {
       return response.data;
     })
